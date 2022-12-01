d4b87fb114ba0dcae4746a536cd75bafa85fbe5f
qbmk <97738007+qbmk@users.noreply.github.com>
2022-12-01 20:50:07 +0200

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-apt list apt list --installed > packages.txt
+apt list --installed > packages.txt
 apt install git
 git clone https://git.savannah.gnu.org/git/grep.git
 ls -alR grep > list.txt
